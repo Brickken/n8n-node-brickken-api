5bce3be4f70cd6055a48a227d208dbfb38048d1c
Maximiliano <maximiliano@brickken.com>
2025-11-28 16:24:26 +0100

    fix: restructure project, clean dependencies, and fix build config

--- dist/nodes/BrickkenApiV2/BrickkenApiV2.node.d.ts
+++ dist/nodes/BrickkenApiV2/BrickkenApiV2.node.d.ts
@@ -1,4 +1,4 @@
-import type { INodeType, INodeTypeDescription } from 'n8n-workflow';
+import type { INodeType, INodeTypeDescription } from "n8n-workflow";
 export declare class BrickkenApiV2 implements INodeType {
     description: INodeTypeDescription;
 }
