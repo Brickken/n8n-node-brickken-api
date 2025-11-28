ffc9abd94412b7a7ef86dac1c58be54a3e0ee73e
Maximiliano <maximiliano@brickken.com>
2025-11-28 17:18:09 +0100

    [n8n]Fixing tsconfig

---
+++ dist/nodes/BrickkenApiV2/BrickkenApiV2.node.d.ts
@@ -0,0 +1,5 @@
+import type { INodeType, INodeTypeDescription } from "n8n-workflow";
+export declare class BrickkenApiV2 implements INodeType {
+    description: INodeTypeDescription;
+}
+//# sourceMappingURL=BrickkenApiV2.node.d.ts.map
