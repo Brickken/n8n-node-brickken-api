cd17bcd3cb77d8581142bf2579ae39e3cac825ce
Maximiliano <82886698+AllenMaxi@users.noreply.github.com>
2025-11-28 16:46:51 +0100

    [n8n]Deploy fixed (#2)

--- dist/credentials/BrickkenApi.credentials.js
+++ dist/credentials/BrickkenApi.credentials.js
@@ -3,47 +3,47 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.BrickkenApi = void 0;
 class BrickkenApi {
     constructor() {
-        this.name = 'brickkenApi';
-        this.displayName = 'Brickken API';
-        this.icon = 'file:brickkenApiV2.svg';
-        this.documentationUrl = 'https://docs.brickken.com';
+        this.name = "brickkenApi";
+        this.displayName = "Brickken API";
+        this.icon = "file:brickkenApiV2.svg";
+        this.documentationUrl = "https://docs.brickken.com";
         this.properties = [
             {
-                displayName: 'API Key',
-                name: 'apiKey',
-                type: 'string',
+                displayName: "API Key",
+                name: "apiKey",
+                type: "string",
                 typeOptions: { password: true },
-                default: '',
+                default: "",
                 required: true,
-                description: 'Brickken API key to authenticate requests',
+                description: "Brickken API key to authenticate requests"
             },
             {
-                displayName: 'Environment',
-                name: 'environment',
-                type: 'options',
+                displayName: "Environment",
+                name: "environment",
+                type: "options",
                 options: [
-                    { name: 'Sandbox', value: 'sandbox' },
-                    { name: 'Production', value: 'production' },
+                    { name: "Sandbox", value: "sandbox" },
+                    { name: "Production", value: "production" }
                 ],
-                default: 'sandbox',
-                description: 'Choose which Brickken API environment to use',
-            },
+                default: "sandbox",
+                description: "Choose which Brickken API environment to use"
+            }
         ];
         this.authenticate = {
-            type: 'generic',
+            type: "generic",
             properties: {
                 headers: {
                     // Pass API key in required header
-                    'x-api-key': '={{$credentials.apiKey}}',
-                },
-            },
+                    "x-api-key": "={{$credentials.apiKey}}"
+                }
+            }
         };
         this.test = {
             request: {
                 baseURL: '={{$credentials.environment === "production" ? "https://api.brickken.com" : "https://api-sandbox.brickken.com"}}',
-                url: '/get-network-info',
-                method: 'GET',
-            },
+                url: "/get-network-info",
+                method: "GET"
+            }
         };
     }
 }
